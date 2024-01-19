efb62c1e51a5569e3169b0cb1702816547d72227
Victor Clima <climav96@gmail.com>
2024-01-19 21:09:09 +0200

    second commit

--- crud-application/src/main/java/com/clima/crudapplication/CrudApplication.java
+++ crud-application/src/main/java/com/clima/crudapplication/CrudApplication.java
@@ -5,6 +5,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrudApplication {
@@ -17,6 +19,16 @@ public class CrudApplication {
 	@Bean
 	public MockService mockService() {
 		return new MockService();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*");
+			}
+		};
 	}
 
 	public static void main(String[] args) {
